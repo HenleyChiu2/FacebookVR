705bb8c752786c57500c9964bb02dc132cb93c5c
Moldypear <Flippersticker@hotmail.com>
2017-04-03 16:53:49 -0500

    Matt's tutorial
    vr movement

---
+++ Assets/CubeInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnGazeEnter(){
+	
+	}
+
+	public void OnGazeExit(){
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 3.0f;
+	Rigidbody rbody;
+
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rbody.velocity = Vector3.zero;
+		rbody.angularVelocity = Vector3.zero;
+	}
+
+	void FixedUpdate () {
+		if(Input.touchCount>0||Input.GetMouseButton(0))
+			rbody.MovePosition(transform.position+transform.forward*Time.deltaTime*speed);
+	}
+}
