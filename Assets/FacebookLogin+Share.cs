70ca46b6a547aa015407d35961ccad08e1641a13
Moldypear <Flippersticker@hotmail.com>
2017-04-06 17:54:53 -0500

    Fixed it
    hopefully

---
+++ Assets/FacebookLogin+Share.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using Facebook.Unity;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookManger : MonoBehaviour {
+
+    public Text userIDText;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+    
+
+
+    public void LogIn()
+    {
+        FB.LogInWithReadPermissions();
+    }
+
+    private void OnLogin(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            AccessToken token = AccessToken.CurrentAccessToken;
+            userIDText.text = token.UserId;
+        }
+    }
+
+    public void Share()
+    {
+        /* Will pop up share */
+        FB.ShareLink(callback:OnShare
+            );
+    }
+
+    private void OnShare(IShareResult result)
+    {
+        if (result.Cancelled || !string.IsNullOrEmpty(result.Error))
+        {
+            Debug.Log("ShareLink error: " + result.Error);
+        }
+        else if (!string.IsNullOrEmpty(result.PostId))
+        {
+            Debug.Log(result.PostId);
+        }
+        else
+            Debug.Log("Share succeeded");
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : WedgeInteraction1 {
+	//this.clicked = clicked; 
+	public float speed = 3.0f;
+	public static Rigidbody rbody;
+
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rbody.velocity = Vector3.zero;
+		rbody.angularVelocity = Vector3.zero;
+	}
+
+	void FixedUpdate () {
+		if (WedgeInteraction1.clicked) {
+			Vector3 cylindarpos = new Vector3 (-16, 2, 10);
+			rbody.MovePosition (cylindarpos); 
+		}
+	}
+}
