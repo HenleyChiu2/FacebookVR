39767f0d445e98db350246004d4c3642bdcb7c77
Lucy Rowland <lucyrowland96@gmail.com>
2017-04-06 18:49:35 -0400

    lookback v.1.0
    EXCEPT YOU GOTTA LOOK DOWN YA FEEL

---
+++ Assets/LookBack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookBack : MonoBehaviour {
+
+	public Transform vrCamera; 
+	public Transform moveCamera; 
+	public float toggleAngle = -30.0f; 
+	public float speed = 3.0f; 
+	public int posX = 0;
+	public int posY = 3; 
+	public int posZ = 0; 
+
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (vrCamera.eulerAngles.x > -30.0f && vrCamera.eulerAngles.x < -90.0f) {
+			Debug.Log ("Movement");
+			moveCamera.transform.position = new Vector3(posX, posY, posZ); 
+		}
+	}
+}
