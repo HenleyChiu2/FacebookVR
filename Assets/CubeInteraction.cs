eca4f1289da71ba39ae30018213664e03f263338
lucyrowland <lucyrowland96@gmail.com>
2017-04-06 01:57:43 -0400

    GAZE INTERACTION IS UP BITCHES

--- Assets/CubeInteraction.cs
+++ Assets/CubeInteraction.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CubeInteraction : MonoBehaviour {
 
-	// Use this for initialization
+	private float timer;
+	public float gazeTime = 2f; 
+	private bool gazedAt; 
+
 	void Start () {
-		
+		//timer = 0; 
 	}
 
 	public void OnGazeEnter(){
@@ -20,6 +24,37 @@ public class CubeInteraction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (gazedAt == true) {
+			timer += Time.deltaTime; 	
+
+
+
+			if (timer >= gazeTime) {
+				ExecuteEvents.Execute (gameObject, new PointerEventData (EventSystem.current), ExecuteEvents.pointerClickHandler);
+
+				Transform child = transform.GetChild (0);
+				Vector3 newScale = new Vector3(2,2,2);
+				child.localScale = newScale; 
+
+				timer = 0f; 
+				GetComponent<Collider> ().enabled = false; 
+
+			}
+		}
+
+
+
+	}
+
+	public void PointerEnter() {
+		//Debug.Log ("Pointer Enter");
+		gazedAt = true; 
+	}
+	public void PointerExit() {
+		//Debug.Log ("Pointer Exit");
+		gazedAt = false; 
+	}
+	public void PointerClick() {
+		Debug.Log ("Pointer Click");
 	}
 }
